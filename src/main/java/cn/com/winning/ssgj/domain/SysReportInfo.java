4bdb27f72913ef4b63472e25ba6e090db0708b27
chensj881008 <chensj881008@yeah.net>
2018-02-02 09:29:06 +0800

    单据报表表增加状态字段status

--- src/main/java/cn/com/winning/ssgj/domain/SysReportInfo.java
+++ src/main/java/cn/com/winning/ssgj/domain/SysReportInfo.java
@@ -1,127 +1,233 @@
 package cn.com.winning.ssgj.domain;
 
 import java.io.Serializable;
-
 import org.apache.ibatis.type.Alias;
-
 import java.util.Date;
-
 import cn.com.winning.ssgj.domain.BaseDomain;
 
 /**
+ * Coder AutoGenerator generate.
  *
- *
- * @author SSGJ
- * @date 2018-01-18 10:11:47
+ * @author Coder AutoGenerator
+ * @date 2018-02-02 09:25:05
  */
-
+ 
 @Alias("sysReportInfo")
 public class SysReportInfo extends BaseDomain implements Serializable {
 
-    private static final long serialVersionUID = -1L;
+	private static final long serialVersionUID = -1L;
 
-    private Long id;
+	/**
+	 * @val 主键ID
+	 */
+	private Long id;
+	
+	/**
+	 * @val 编码
+	 */
+	private String reportCode;
+	
+	/**
+	 * @val 名称
+	 */
+	private String reportName;
+	
+	/**
+	 * @val 类型 0 凭条;1 发票;2 缴款;3 缴款单据;4 单据;5 台账;6 处方医嘱;7 申请单;8 治疗单据;9 医疗文书;10 临时医嘱;11 报告调阅;12 巡视单;13 报表;
+	 */
+	private Integer reportType;
+	
+	/**
+	 * @val 纸张类型 
+	 */
+	private Integer paperFormat;
+	
+	/**
+	 * @val 生产应用场景 适用于单据等，不包含报表
+	 */
+	private String prdUseScope;
+	
+	/**
+	 * @val 生产使用者,适用于报表中
+	 */
+	private String prdUser;
+	
+	/**
+	 * @val 描述
+	 */
+	private String repoetDesc;
+	
+	/**
+	 * @val 状态 0 失效 1生效
+	 */
+	private Integer status;
+	
+	/**
+	 * @val 维护人员
+	 */
+	private Long lastUpdator;
+	
+	/**
+	 * @val 维护时间
+	 */
+	private Date lastUpdateTime;
+	
+	public SysReportInfo() {
 
-    private String reportCode;
+	}
 
-    private String reportName;
-
-    private Integer reportType;
-
-    private Integer paperFormat;
-
-    private String prdUseScope;
-
-    private String prdUser;
-
-    private String repoetDesc;
-
-    private Long lastUpdator;
-
-    private Date lastUpdateTime;
-
-    public SysReportInfo() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getReportCode() {
-        return reportCode;
-    }
-
-    public void setReportCode(String reportCode) {
-        this.reportCode = reportCode;
-    }
-
-    public String getReportName() {
-        return reportName;
-    }
-
-    public void setReportName(String reportName) {
-        this.reportName = reportName;
-    }
-
-    public Integer getReportType() {
-        return reportType;
-    }
-
-    public void setReportType(Integer reportType) {
-        this.reportType = reportType;
-    }
-
-    public Integer getPaperFormat() {
-        return paperFormat;
-    }
-
-    public void setPaperFormat(Integer paperFormat) {
-        this.paperFormat = paperFormat;
-    }
-
-    public String getPrdUseScope() {
-        return prdUseScope;
-    }
-
-    public void setPrdUseScope(String prdUseScope) {
-        this.prdUseScope = prdUseScope;
-    }
-
-    public String getPrdUser() {
-        return prdUser;
-    }
-
-    public void setPrdUser(String prdUser) {
-        this.prdUser = prdUser;
-    }
-
-    public String getRepoetDesc() {
-        return repoetDesc;
-    }
-
-    public void setRepoetDesc(String repoetDesc) {
-        this.repoetDesc = repoetDesc;
-    }
-
-    public Long getLastUpdator() {
-        return lastUpdator;
-    }
-
-    public void setLastUpdator(Long lastUpdator) {
-        this.lastUpdator = lastUpdator;
-    }
-
-    public Date getLastUpdateTime() {
-        return lastUpdateTime;
-    }
-
-    public void setLastUpdateTime(Date lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
-    }
-
+	/**
+	 * @val 主键ID
+	 */
+	public Long getId() {
+		return id;
+	}
+	
+	/**
+	 * @val 主键ID
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @val 编码
+	 */
+	public String getReportCode() {
+		return reportCode;
+	}
+	
+	/**
+	 * @val 编码
+	 */
+	public void setReportCode(String reportCode) {
+		this.reportCode = reportCode;
+	}
+	
+	/**
+	 * @val 名称
+	 */
+	public String getReportName() {
+		return reportName;
+	}
+	
+	/**
+	 * @val 名称
+	 */
+	public void setReportName(String reportName) {
+		this.reportName = reportName;
+	}
+	
+	/**
+	 * @val 类型 0 凭条;1 发票;2 缴款;3 缴款单据;4 单据;5 台账;6 处方医嘱;7 申请单;8 治疗单据;9 医疗文书;10 临时医嘱;11 报告调阅;12 巡视单;13 报表;
+	 */
+	public Integer getReportType() {
+		return reportType;
+	}
+	
+	/**
+	 * @val 类型 0 凭条;1 发票;2 缴款;3 缴款单据;4 单据;5 台账;6 处方医嘱;7 申请单;8 治疗单据;9 医疗文书;10 临时医嘱;11 报告调阅;12 巡视单;13 报表;
+	 */
+	public void setReportType(Integer reportType) {
+		this.reportType = reportType;
+	}
+	
+	/**
+	 * @val 纸张类型 
+	 */
+	public Integer getPaperFormat() {
+		return paperFormat;
+	}
+	
+	/**
+	 * @val 纸张类型 
+	 */
+	public void setPaperFormat(Integer paperFormat) {
+		this.paperFormat = paperFormat;
+	}
+	
+	/**
+	 * @val 生产应用场景 适用于单据等，不包含报表
+	 */
+	public String getPrdUseScope() {
+		return prdUseScope;
+	}
+	
+	/**
+	 * @val 生产应用场景 适用于单据等，不包含报表
+	 */
+	public void setPrdUseScope(String prdUseScope) {
+		this.prdUseScope = prdUseScope;
+	}
+	
+	/**
+	 * @val 生产使用者,适用于报表中
+	 */
+	public String getPrdUser() {
+		return prdUser;
+	}
+	
+	/**
+	 * @val 生产使用者,适用于报表中
+	 */
+	public void setPrdUser(String prdUser) {
+		this.prdUser = prdUser;
+	}
+	
+	/**
+	 * @val 描述
+	 */
+	public String getRepoetDesc() {
+		return repoetDesc;
+	}
+	
+	/**
+	 * @val 描述
+	 */
+	public void setRepoetDesc(String repoetDesc) {
+		this.repoetDesc = repoetDesc;
+	}
+	
+	/**
+	 * @val 状态 0 失效 1生效
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+	
+	/**
+	 * @val 状态 0 失效 1生效
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	/**
+	 * @val 维护人员
+	 */
+	public Long getLastUpdator() {
+		return lastUpdator;
+	}
+	
+	/**
+	 * @val 维护人员
+	 */
+	public void setLastUpdator(Long lastUpdator) {
+		this.lastUpdator = lastUpdator;
+	}
+	
+	/**
+	 * @val 维护时间
+	 */
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+	
+	/**
+	 * @val 维护时间
+	 */
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+	
 }
