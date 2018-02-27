9b2f043c79cdb62a1f33acaead653f5ea0101912
chenshijie1988 <chenshijie1988@yeah.net>
2018-02-27 20:20:21 +0800

    代码修复

---
+++ src/main/java/cn/com/winning/ssgj/service/EtTrainVideoListService.java
@@ -0,0 +1,11 @@
+package cn.com.winning.ssgj.service;
+
+/**
+ * @author chenshijie
+ * @title
+ * @email chensj@winning.com.cm
+ * @package cn.com.winning.ssgj.service
+ * @date 2018-02-27 20:08
+ */
+public interface EtTrainVideoListService {
+}
---
+++ src/main/java/cn/com/winning/ssgj/service/EtTrainVideoRecordService.java
@@ -0,0 +1,11 @@
+package cn.com.winning.ssgj.service;
+
+/**
+ * @author chenshijie
+ * @title
+ * @email chensj@winning.com.cm
+ * @package cn.com.winning.ssgj.service
+ * @date 2018-02-27 20:09
+ */
+public interface EtTrainVideoRecordService {
+}
