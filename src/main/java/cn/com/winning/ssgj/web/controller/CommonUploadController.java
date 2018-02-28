f13ec2d2f644fb2ec7c08661be43ede89aed65e5
chensj881008 <chensj881008@yeah.net>
2018-02-28 19:13:43 +0800

    提交培训视频代码

---
+++ src/main/java/cn/com/winning/ssgj/web/controller/CommonUploadController.java
@@ -0,0 +1,11 @@
+package cn.com.winning.ssgj.web.controller;
+
+/**
+ * @author chenshijie
+ * @title
+ * @email chensj@winning.com.cm
+ * @package cn.com.winning.ssgj.web.controller
+ * @date 2018-02-28 14:08
+ */
+public class CommonUploadController {
+}
