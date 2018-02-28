8053c242230549d1e9d3fa6346cb2e826732bfc4
chensj881008 <chensj881008@yeah.net>
2018-02-28 12:35:58 +0800

    add pdm

---
+++ src/main/java/cn/com/winning/ssgj/web/controller/SysTrainVideoRepoController.java
@@ -0,0 +1,11 @@
+package cn.com.winning.ssgj.web.controller;
+
+/**
+ * @author chenshijie
+ * @title
+ * @email chensj@winning.com.cm
+ * @package cn.com.winning.ssgj.web.controller
+ * @date 2018-02-28 10:39
+ */
+public class SysTrainVideoRepoController {
+}
