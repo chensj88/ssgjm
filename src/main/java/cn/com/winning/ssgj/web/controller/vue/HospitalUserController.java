2cba888af1abf775c27c2c817be024d2b8bf329b
chensj881008 <chensj881008@yeah.net>
2018-03-12 17:33:21 +0800

    add

--- src/main/java/cn/com/winning/ssgj/web/controller/vue/HospitalUserController.java
+++ src/main/java/cn/com/winning/ssgj/web/controller/vue/HospitalUserController.java
@@ -2,6 +2,8 @@ package cn.com.winning.ssgj.web.controller.vue;
 
 import cn.com.winning.ssgj.base.Constants;
 import cn.com.winning.ssgj.domain.SysUserInfo;
+import cn.com.winning.ssgj.domain.support.Row;
+import cn.com.winning.ssgj.web.controller.common.BaseController;
 import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,19 +23,27 @@ import java.util.Map;
  */
 @CrossOrigin
 @Controller
-@RequestMapping("/vue/processManager")
-public class HospitalUserController {
+@RequestMapping("/vue/hospital")
+public class HospitalUserController extends BaseController {
 
     @RequestMapping(value = "/list.do")
     @ResponseBody
     public Map<String,Object> queryHospitalUserInfo(){
 
-        SysUserInfo user = (SysUserInfo) SecurityUtils.getSubject().getPrincipal();
-
+       /* SysUserInfo user = (SysUserInfo) SecurityUtils.getSubject().getPrincipal();
+        long c_id = (long) user.getMap().get("C_ID");*/
+        long c_id = 9879L;
+        SysUserInfo queryUser =  new SysUserInfo();
+        Row row = new Row(0,10);
+        queryUser.setRow(row);
+        queryUser.setSsgs(c_id);
+        queryUser.setUserType(Constants.User.USER_TYPE_HOSPITAL);
+        List<SysUserInfo> queryUserList = super.getFacade().getSysUserInfoService().getSysUserInfoPaginatedList(queryUser);
+        int total = super.getFacade().getSysUserInfoService().getSysUserInfoCount(queryUser);
         Map<String,Object> result = new HashMap<String,Object>();
-      /*  result.put("total", total);
+        result.put("total", total);
         result.put("status", Constants.SUCCESS);
-        result.put("rows", );*/
+        result.put("rows", queryUserList);
         return result;
 
     }
