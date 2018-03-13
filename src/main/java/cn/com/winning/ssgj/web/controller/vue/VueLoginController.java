8acdc05d4e97ea311fe16778990832a50605cca8
chensj881008 <chensj881008@yeah.net>
2018-03-13 16:53:42 +0800

    aa

--- src/main/java/cn/com/winning/ssgj/web/controller/vue/VueLoginController.java
+++ src/main/java/cn/com/winning/ssgj/web/controller/vue/VueLoginController.java
@@ -1,5 +1,9 @@
 package cn.com.winning.ssgj.web.controller.vue;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author chenshijie
  * @title
@@ -7,5 +11,11 @@ package cn.com.winning.ssgj.web.controller.vue;
  * @package cn.com.winning.ssgj.web.controller.vue
  * @date 2018-03-13 16:27
  */
+@CrossOrigin
+@Controller
+@RequestMapping(value = "/vue/login")
 public class VueLoginController {
+
+
+
 }
