9577c29b0c292101be6d719d386eb5bc60d3d8e6
chensj881008 <chensj881008@yeah.net>
2018-03-13 16:52:48 +0800

    delete test resources

---
+++ src/main/java/cn/com/winning/ssgj/web/controller/vue/VueLoginController.java
@@ -0,0 +1,11 @@
+package cn.com.winning.ssgj.web.controller.vue;
+
+/**
+ * @author chenshijie
+ * @title
+ * @email chensj@winning.com.cm
+ * @package cn.com.winning.ssgj.web.controller.vue
+ * @date 2018-03-13 16:27
+ */
+public class VueLoginController {
+}
