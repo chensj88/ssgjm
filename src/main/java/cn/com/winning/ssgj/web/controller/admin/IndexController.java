1cc42fd9539fdc24fff93c082530a0f2c284794c
chensj881008 <chenshijie1988@yeah.net>
2018-04-26 16:58:43 +0800

    1、首页页面报表展示

--- src/main/java/cn/com/winning/ssgj/web/controller/admin/IndexController.java
+++ src/main/java/cn/com/winning/ssgj/web/controller/admin/IndexController.java
@@ -1,12 +1,19 @@
 package cn.com.winning.ssgj.web.controller.admin;
 
+import cn.com.winning.ssgj.base.Constants;
 import cn.com.winning.ssgj.base.annoation.ILog;
+import cn.com.winning.ssgj.domain.*;
 import cn.com.winning.ssgj.web.controller.common.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author chenshijie
@@ -23,4 +30,45 @@ public class IndexController extends BaseController {
     public String indexPageInfo(HttpServletRequest request, Model model) {
         return "index/index";
     }
+
+    @RequestMapping(value = "/count.do")
+    @ResponseBody
+    public Map<String,Object> queryPmisInfo(){
+        List<String> itemData = new ArrayList<>();
+        List<Integer> numData = new ArrayList<>();
+        PmisContractInfo contractInfo = new PmisContractInfo();
+        int htNum = super.getFacade().getPmisContractInfoService().getPmisContractInfoCount(contractInfo);
+        itemData.add("合同信息");
+        numData.add(htNum);
+        PmisProjectBasicInfo basicInfo = new PmisProjectBasicInfo();
+        int pNum = super.getFacade().getPmisProjectBasicInfoService().getPmisProjectBasicInfoCount(basicInfo);
+        itemData.add("项目信息");
+        numData.add(pNum);
+        PmisProductInfo info = new PmisProductInfo();
+        int ppNum = super.getFacade().getPmisProductInfoService().getPmisProductInfoCount(info );
+        itemData.add("产品信息");
+        numData.add(ppNum);
+        SysUserInfo user = new SysUserInfo();
+        user.setUserType(Constants.User.USER_TYPE_COMPANY);
+        user.setStatus(1);
+        int userNum = super.getFacade().getSysUserInfoService().getSysUserInfoCount(user);
+        itemData.add("医院用户信息");
+        numData.add(userNum);
+        user.setUserType(Constants.User.USER_TYPE_HOSPITAL);
+        user.setStatus(1);
+        userNum = super.getFacade().getSysUserInfoService().getSysUserInfoCount(user);
+        itemData.add("医院用户信息");
+        numData.add(userNum);
+        EtProcessManager manager = new EtProcessManager();
+        int pmNum = super.getFacade().getEtProcessManagerService().getEtProcessManagerCount(manager);
+        itemData.add("系统统计项目");
+        numData.add(pmNum);
+        Map<String,Object> result = new HashMap<String,Object>();
+        result.put("status", Constants.SUCCESS);
+        result.put("itemData",itemData);
+        result.put("numData",numData);
+        return result;
+
+    }
+
 }
--- src/main/webapp/resources/js/index/index.js
+++ src/main/webapp/resources/js/index/index.js
@@ -1,209 +1,100 @@
 $(function () {
-    function drawPieChart(placeholder, data, position) {
-        $.plot(placeholder, data, {
-            series: {
-                pie: {
+
+    $.ajax({
+        url: Common.getRootPath() + '/home/count.do',
+        type: "post",
+        dataType: 'json',
+        async: false,
+        cache: false,
+        success: function (data) {
+            if(data.status == 'success'){
+                initEcharts(data.itemData,data.numData);
+            }
+        }
+    });
+    function initEcharts(itemData,numData){
+        var dataStyle = {
+            normal: {
+                label: {
                     show: true,
-                    tilt:0.8,
-                    highlight: {
-                        opacity: 0.25
-                    },
-                    stroke: {
-                        color: '#fff',
-                        width: 2
-                    },
-                    startAngle: 2
+                    position: 'inside',
                 }
             },
+            borderColor: '#f00',
+            borderWidth: 10,
+            borderType: 'solid'
+        };
+        var option = {
+            tooltip: {
+                show: true,
+                trigger: 'item'
+            },
             legend: {
-                show: true,
-                position: position || "ne",
-                labelBoxBorderColor: null,
-                margin:[-30,15]
-            }
-            ,
-            grid: {
-                hoverable: true,
-                clickable: true
-            }
-        })
-    }
-    var data = [];
-    var placeholder = $('#piechart-placeholder').css({'width':'90%' , 'min-height':'150px'});
-    $.ajax({
-        url : Common.getRootPath() + '/admin/user/count.do',
-        type:'post',
-        cache : false,
-        dataType: 'json',
-        async: false,
-        success :function (result) {
-            var _result = eval(result);
-            if(_result.status == Common.SUCCESS){
-                data = _result.data;
-                drawPieChart(placeholder,_result.data);
-                placeholder.data('chart', _result.data);
-                placeholder.data('draw', drawPieChart);
-            }
-        }
-    });
-
-    $.ajax({
-        url : Common.getRootPath() + '/admin/user/count.do',
-        type:'post',
-        cache : false,
-        dataType: 'json',
-        async: false,
-        success :function (result) {
-            var _result = eval(result);
-            if(_result.status == Common.SUCCESS){
-                data = _result.data;
-                drawPieChart(placeholder,_result.data);
-                placeholder.data('chart', _result.data);
-                placeholder.data('draw', drawPieChart);
-            }
-        }
-    });
-    $.ajax({
-        url : Common.getRootPath() + '/home/loadMenu.do',
-        type:'post',
-        cache : false,
-        dataType: 'json',
-        async: false,
-        success :function (result) {
-            var _result = eval(result);
-            if(_result.status == Common.SUCCESS){
-                var menu = _result.data;
-                initMenu(menu);
-            }
-        }
-    });
-
-    function initMenu(data) {
-        $.each(data,function (index,value,array) {
-            var content = '';
-            if( index == 0 ){
-                content = "<li class=\"active\" >\n" +
-                    "                    <a href=\""+Common.getRootPath()+value.urlPath+"\">\n" +
-                    "                        <i class=\""+value.nodeIcon+"\"></i>\n" +
-                    "                        <span class=\"menu-text\"> "+value.text+" </span>\n" +
-                    "                    </a>\n" +
-                    "                </li>";
-            }else{
-                content = "<li >\n" +
-                    "                    <a href=\"#\" class=\"dropdown-toggle\">\n" +
-                    "                        <i class=\""+value.nodeIcon+"\"></i>\n" +
-                    "                        <span class=\"menu-text\"> "+value.text+" </span>\n" +
-                    "                        <b class=\"arrow icon-angle-down\"></b>"+
-                    "                    </a>\n"+
-                    "                    <ul class=\"submenu\">";
-                    $.each(value.nodes,function (cindex,cvalue,carray) {
-                        content += "<li><a href=\""+Common.getRootPath()+cvalue.urlPath+"\">" +
-                            "        <i class=\""+cvalue.nodeIcon+"\"></i>"+cvalue.text+"</a></li>";
-                    });
-
-                content +=    "</ul></li>";
-            }
-           /* console.log(content);*/
-            $('#nav').append(content);
-        });
-
-    }
-
-    let  itemData = [];
-    let  completeData = [];
-    $.each(data,function (index,value,array) {
-        var content =
-            "<div class=\"grid3\">\n" +
-            "                <span class=\"grey\">\n" +
-            "                <i class=\"icon-facebook-sign icon-2x blue\"></i>\n" +
-            "                &nbsp; "+value.label+"\n" +
-            "            </span>\n" +
-            "            <h4 class=\"bigger pull-right\">"+value.data+"</h4>\n" +
-            "            </div>";
-        $('#userContent').append(content);
-        itemData.push(value.label);
-        completeData.push(value.data);
-    });
-    let dataStyle = {
-        normal: {
-            label: {
-                show: true,
-                position: 'inside',
-            }
-        },
-        borderColor: '#f00',
-        borderWidth: 10,
-        borderType: 'solid'
-    };
-    let option = {
-        tooltip: {
-            show: true,
-            trigger: 'item'
-        },
-        legend: {
-            data: ['数量'],
-            textStyle: {
-                color: '#666'
-            }
-        },
-        grid: {
-            left: '10%',
-            right: '10%',
-            bottom: '10%',
-            containLabel: true
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                mark: {show: true},
-                magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
-                restore: {show: true},
-                saveAsImage: {show: true}
-            }
-        },
-        calculable: true,
-        xAxis: [
-            {
-                type: 'category',
-                data:itemData,
-                axisTick: {
-                    alignWithLabel: true
-                },
-                axisLabel: {
-                    color: '#666666',
-                    fontFamily: 'Microsoft YaHei'
-                },
-                axisLine: {
-                    show: false
+                data: ['数量'],
+                textStyle: {
+                    color: '#666'
                 }
-            }
-        ],
-        yAxis: [
-            {
-                type: 'value',
-                axisLabel: {
-                    color: '#93abba'
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: '#93abba'
+            },
+            grid: {
+                left: '10%',
+                right: '10%',
+                bottom: '10%',
+                containLabel: true
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: {show: true},
+                    magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                    restore: {show: true},
+                    saveAsImage: {show: true}
+                }
+            },
+            calculable: true,
+            xAxis: [
+                {
+                    type: 'category',
+                    data:itemData,
+                    axisTick: {
+                        alignWithLabel: true
+                    },
+                    axisLabel: {
+                        color: '#666666',
+                        fontFamily: 'Microsoft YaHei'
+                    },
+                    axisLine: {
+                        show: false
                     }
                 }
-            }
-        ],
-        series: [
-            {
-                name: '人员数量',
-                type: 'bar',
-                stack: '总量',
-                itemStyle: dataStyle,
-                data: completeData
-            }
-        ],
-        color: ['#7537ed', '#ba9bf6']
-    };
-    setTimeout(_ => {
-        let myChart = echarts.init(document.getElementById('main'));
-        myChart.setOption(option);
-    }, 0);
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    axisLabel: {
+                        color: '#93abba'
+                    },
+                    axisLine: {
+                        lineStyle: {
+                            color: '#93abba'
+                        }
+                    }
+                }
+            ],
+            series: [
+                {
+                    name: '数量',
+                    type: 'bar',
+                    stack: '总量',
+                    itemStyle: dataStyle,
+                    data: numData
+                }
+            ],
+            color: ['#7537ed', '#ba9bf6']
+        };
+        setTimeout(_ => {
+            var myChart = echarts.init(document.getElementById('main'),'macarons');
+            myChart.setOption(option);
+        }, 0);
+    }
+
 });
