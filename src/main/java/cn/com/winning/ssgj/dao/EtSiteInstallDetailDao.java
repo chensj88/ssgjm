8710fd7278eb2e3c030ecf94925130a8126f6c14
Evol <798615946@qq.com>
2018-05-11 15:22:59 +0800

    上线支持人员后端代码修改

--- src/main/java/cn/com/winning/ssgj/dao/EtSiteInstallDetailDao.java
+++ src/main/java/cn/com/winning/ssgj/dao/EtSiteInstallDetailDao.java
@@ -1,7 +1,10 @@
 package cn.com.winning.ssgj.dao;
 
+import cn.com.winning.ssgj.domain.EtDepartment;
 import cn.com.winning.ssgj.domain.EtSiteInstallDetail;
 import cn.com.winning.ssgj.dao.EntityDao;
+
+import java.util.List;
 
 /**
  *
@@ -10,5 +13,7 @@ import cn.com.winning.ssgj.dao.EntityDao;
  * @date 2018-01-18 10:11:47
  */
 public interface EtSiteInstallDetailDao extends EntityDao<EtSiteInstallDetail> {
+
+    List<EtSiteInstallDetail> getSiteListByDeptId(EtDepartment etDepartment);
 
 }
