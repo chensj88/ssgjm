2516c76388d899fc494cdc8df1755271b9f5217d
chenkuai2017 <827995732@qq.com>
2018-06-11 11:05:46 +0800

    工作站点 名称存ID

---
+++ src/main/java/cn/com/winning/ssgj/web/controller/mobile/MobileTempSiteQuestionController.java
@@ -0,0 +1,20 @@
+package cn.com.winning.ssgj.web.controller.mobile;
+
+import cn.com.winning.ssgj.web.controller.common.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/mobile/tempSiteQuestion")
+public class MobileTempSiteQuestionController  extends BaseController {
+
+
+
+
+
+
+
+
+}
