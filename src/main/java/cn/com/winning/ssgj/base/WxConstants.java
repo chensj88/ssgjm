076cf38c9ba688009e2a7d03debf4da2ace6facf
chenkuai2017 <827995732@qq.com>
2018-06-19 17:59:19 +0800

    chenkuai

--- src/main/java/cn/com/winning/ssgj/base/WxConstants.java
+++ src/main/java/cn/com/winning/ssgj/base/WxConstants.java
@@ -13,7 +13,7 @@ public class WxConstants implements Serializable {
     public static final String CORPID = "wxac9ca7b3c2c43e81";
     public static final String AGENTID = "1000014";
     public static final String SECRET = "X8KHKKb0O3yR7qcnQSFDzBGiPhc8urJBK5sAnUE7";
-    public static final String URL = "http://ssgj.winning-health.com.cn:8081";
+    public static final String URL = "http://ssgj.winning-health.com.cn:8081/ssgjm/mobile/tempSiteQuestion/index.do";
     public static final String STATE = "STATE";
     public static final OauthScope SCOPE =OauthScope.SNSAPI_USERINFO;
 
--- src/main/java/cn/com/winning/ssgj/web/controller/mobile/WxController.java
+++ src/main/java/cn/com/winning/ssgj/web/controller/mobile/WxController.java
@@ -4,6 +4,7 @@ import cn.com.winning.ssgj.base.WxConstants;
 import cn.com.winning.ssgj.base.annoation.ILog;
 import cn.com.winning.ssgj.base.util.WeixinUtil;
 import cn.com.winning.ssgj.web.controller.common.BaseController;
+import cn.com.winning.ssgj.ws.job.ImportPmisDataJob;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.fastweixin.api.enums.OauthScope;
@@ -11,6 +12,7 @@ import com.fastweixin.company.api.QYOauthAPI;
 import com.fastweixin.company.api.config.QYAPIConfig;
 import com.fastweixin.util.BeanUtil;
 import com.fastweixin.util.StrUtil;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +26,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.logging.Logger;
 
 /**
  * 企业微信访问操作
@@ -32,64 +35,29 @@ import java.net.URLEncoder;
 @CrossOrigin
 @RequestMapping("/mobile/wxInfo")
 public class WxController extends BaseController {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(WxController.class);
+
 
     @RequestMapping(value = "/list.do")
     @ILog
     public String list(Model model,String code, String floorName, String serialNo, String userId) {
+        String userInfo = null;
         try{
             //获取企业的access_token
             String token = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wxac9ca7b3c2c43e81&corpsecret=X8KHKKb0O3yR7qcnQSFDzBGiPhc8urJBK5sAnUE7-j8";
             JSONObject apiAccessToken= WeixinUtil.getApiReturn(token);
             String access_token = (String)apiAccessToken.get("access_token");
-
-
-//            URL url = new URL( "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wxac9ca7b3c2c43e81&corpsecret=X8KHKKb0O3yR7qcnQSFDzBGiPhc8urJBK5sAnUE7-j8");
-//            //AccessToken at = WeixinUtil.getAccessToken(appId, appSecret);
-//            HttpURLConnection urlcon = (HttpURLConnection)url.openConnection();
-//            InputStream is = urlcon.getInputStream();
-//            BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
-//            StringBuffer bs = new StringBuffer();
-//            String l = null;
-//            while((l=buffer.readLine())!=null){
-//                bs.append(l);
-//            }
-//            //获取access_token
-//            JSONObject firstWx = JSONObject.parseObject(bs.toString());
-//            String access_token = (String)firstWx.get("access_token");
-            //QYOauthAPI qYOauthAPI= new QYOauthAPI(QYAPIConfig);
             //获取成员信息
             String redirectUrl = WxConstants.URL;
             OauthScope snsapi_base = WxConstants.SCOPE;
             String state =WxConstants.STATE;
-            String userInfo= WeixinUtil.getOauthPageUrl(redirectUrl,snsapi_base,state);
-            JSONObject apiUserInfo= WeixinUtil.getApiReturn(userInfo);
-            String code_info = (String)apiUserInfo.get("code");
-
-
-
-//            URL url2 = new URL(sss );
-//            HttpURLConnection urlcon2 = (HttpURLConnection)url.openConnection();
-//            InputStream is2 = urlcon.getInputStream();
-//            BufferedReader buffer2 = new BufferedReader(new InputStreamReader(is));
-//            StringBuffer bs2 = new StringBuffer();
-//            String ll = null;
-//            while((ll=buffer2.readLine())!=null){
-//                bs2.append(ll);
-//            }
-//            //获取access_token
-//            JSONObject firstWx2 = JSONObject.parseObject(bs2.toString());
-//            String code2 = (String)firstWx2.get("code");
-
-            //缓存acctss_token
-            //获取登录成员信息
-            model.addAttribute("code",code_info);
+            userInfo= WeixinUtil.getOauthPageUrl(redirectUrl,snsapi_base,state);
 
         }catch (Exception e){
             e.printStackTrace();
         }
 
-
-        return "/mobile/wx-home";
+        return "redirect:" + userInfo;
     }
 
 
