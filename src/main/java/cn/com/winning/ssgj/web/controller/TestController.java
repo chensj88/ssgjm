2c8b7db098052dfbb4f6ed6d4e739f5d3a9ca5af
chenshijie1988 <chenshijie1988@yeah.net>
2018-07-11 22:32:41 +0800

    业务流程信息生成修改 1、业务流程生成时候自动确定是否需要生成配置信息 2、流程配置信息创建是否自动更新业务流程信息中的数据

--- src/main/java/cn/com/winning/ssgj/web/controller/TestController.java
+++ src/main/java/cn/com/winning/ssgj/web/controller/TestController.java
@@ -53,8 +53,9 @@ public class TestController extends BaseController{
     }
     @RequestMapping(value = "/upload.do")
     public String upload(){
-        return "test/upload";
+        return "test/scriptPage";
     }
+
 
     @RequestMapping(value = "/exec.do")
     @ResponseBody
