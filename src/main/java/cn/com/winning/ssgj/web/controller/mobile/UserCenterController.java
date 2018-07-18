e72856fd1f0ecb31e201a1e3aa22abaf138f8a5a
chenkuai2017 <827995732@qq.com>
2018-07-18 09:51:29 +0800

    chenkuai

--- src/main/java/cn/com/winning/ssgj/web/controller/mobile/UserCenterController.java
+++ src/main/java/cn/com/winning/ssgj/web/controller/mobile/UserCenterController.java
@@ -6,6 +6,7 @@ import cn.com.winning.ssgj.domain.SysOrganization;
 import cn.com.winning.ssgj.web.controller.common.BaseController;
 import com.alibaba.fastjson.JSONObject;
 
+import net.sf.json.JSONArray;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -26,22 +27,28 @@ public class UserCenterController extends BaseController {
      * @return
      */
     @RequestMapping(value="/list.do")
-    public String userCenter(Model model ,String parameter,String userId,String serialNo,String serialName){
+    public String userCenter(Model model ,String parameter,Long userId,String serialNo,String serialName,String isManager){
         String access_token = super.getAccessToken();
-        userId ="5823";
-        serialNo ="11403";
-        serialName ="通州市医院";
+//        userId ="5823";
+//        serialNo ="11403";
+//        serialName ="通州市医院";
+
+        String userId_str = super.id_user(userId,"1");
         StringBuffer stringBuffer = new StringBuffer(WxConstants.QY_USER_INFO2);
-        stringBuffer.append("access_token=").append(access_token).append("&userid=").append(userId);
+        stringBuffer.append("access_token=").append(access_token).append("&userid=").append(userId_str);
         try{
             JSONObject userInfo = WeixinUtil.getApiReturn(stringBuffer.toString());
             //获取部门信息
             SysOrganization organization = new SysOrganization();
-            String departId = (String) userInfo.get("department").toString();
-            departId =departId.replaceAll("\\[","");
-            departId = departId.replaceAll("\\]","");
-            organization.setId(Long.valueOf(departId));
-            organization = super.getFacade().getSysOrganizationService().getSysOrganization(organization);
+            JSONArray departIdJson = JSONArray.fromObject(userInfo.get("department") );
+            if(departIdJson.size()>0){
+                int departId = (int) departIdJson.get(0);
+//              departId =departId.replaceAll("\\[","");
+//              departId = departId.replaceAll("\\]","");
+                organization.setId((long)departId);
+                organization = super.getFacade().getSysOrganizationService().getSysOrganization(organization);
+
+            }
 
             String user_name = (String)userInfo.get("name");
             user_name =new String(user_name.getBytes(),"UTF-8");
@@ -50,7 +57,8 @@ public class UserCenterController extends BaseController {
             model.addAttribute("department_name",organization.getJgqc());
             model.addAttribute("serialNo",serialNo);
             model.addAttribute("serialName",serialName);
-
+            model.addAttribute("isManager",isManager);
+            model.addAttribute("userId",userId);
 
         }catch (Exception e){
             e.printStackTrace();
