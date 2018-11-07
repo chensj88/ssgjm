788181ab23c6fc79cedc56df80290280eb972405
chensj881008 <chenshijie1988@yeah.net>
2018-11-07 09:43:33 +0800

    修改显示字体

--- src/main/webapp/resources/js/auth/auth.js
+++ src/main/webapp/resources/js/auth/auth.js
@@ -1,14 +1,12 @@
 /**
  * 用户按钮权限检查
- *
+ * 根据访问URL和当前人员来查询按钮权限
  * @param btnType
  */
 function checkUserButtonAuth(btnType){
     var ctrl = Common.getControllerPath();
     var rootUrl = Common.getRootPath();
     var pageUrl = ctrl.substring(rootUrl.length,ctrl.length-1);
-    console.log(ctrl);
-    console.log(rootUrl);
     $.ajax({
         type: "POST",
         url: Common.getRootPath() + "/auth/initBtnList.do",
